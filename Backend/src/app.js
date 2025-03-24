0ca89424b13608510bbe8f444115d43c301648ca
Danesh <techydanesh@gmail.com>
2025-03-24 19:37:06 +0530

    configured cors attributes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,11 @@ const userRoutes = require("./routes/user.routes");
 const cors = require("cors");
 const app = express();
 // connectToDB();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // For local testing
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+}));
 app.use(express.json());
 
 app.get("/home", (req, res) => {
